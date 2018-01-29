db30750f19f0dee23a0576325e12b947e108b316
eliza29 <elizaseguraf@gmail.com>
2018-01-28 22:16:07 -0500

    subiendo estilos css

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ $(document).ready(()=>{
       image.wrap('<figure></figure>');
       image.after('<figcaption></figcaption>');
       image.next().text(image.attr('alt')); 
-      $('figure').addClass('card text-white bg-success col-sm-7');  
+      $('figure').addClass('card text-white bg-success col-sm-8 offset-sm-2'); 
     });
  
     return this;
