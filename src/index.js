604cf7e2a3e24ff881ae1012e6912f8d4d52d41e
Azur_Azabache <tharuka_20@hotmail.com>
2018-01-30 19:40:43 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
 $(document).ready(()=>{
-  // $.fn.cambiaColor = function(options) {
-  //   const configuration = $.extend({
-  //     color: 'red',
-  //   }, options);
-  //   this.each((indice, elemento)=>{
-  //     let color = configuration.color;
-  //     $(elemento).css('color', color);
-  //   });
-  // };
-  // $('.container').find('p').cambiaColor({color: 'brown'});
-  
+
   $.fn.cardify = function() {
-    this.each(function() {
+    this.filter("img").each(function() {
       let image = $(this);
       image.wrap('<figure></figure>');
       image.after('<figcaption></figcaption>');
-      image.next().text(image.attr('alt')); 
-      $('figure').addClass('card text-white bg-success col-sm-8 offset-sm-2'); 
+      image.next().text(image.attr('alt'));
+      $('figure').addClass('card text-white bg-success col-sm-8 offset-sm-2');
+      $('figcaption').addClass('center');
+      $('img').addClass('top');
     });
- 
+  $("img").hover(function() {
+  $(this).addClass('effect');
+});
     return this;
   };
- 
-  // Usage example:
-  // $('img').cardify();
+
 });
